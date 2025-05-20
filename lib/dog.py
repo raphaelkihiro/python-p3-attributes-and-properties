1d81be9d4a6fedfd5a97b6fc2e831d484aec9572
raphaelkihiro <kihirongigi@gmail.com>
2025-05-20 23:41:21 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua",
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer"
+]
+
 class Dog:
     def __init__(self,name,breed="Mutt"):
         self.name=name
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+APPROVED_JOBS = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
+]
+
 class Person:
+    pass
+
     def __init__(self,name):
         self.name=name
